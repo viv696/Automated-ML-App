b5b9443fe9a8cee0790ec583b97b3c1e7f837288
viv696 <barsopia.vivek@gmail.com>
2024-04-25 00:05:55 +0530

    intentional issue

--- app.py
+++ app.py
@@ -22,6 +22,9 @@ if choice == "Upload":
         df.to_csv(SOURCE_DATA, index=None)
         st.dataframe(df)
 
+if choice == "EDA":
+    pass
+
 if choice == "Modelling":
     st.title("Machine Learning")
     target = st.selectbox("Choose your Target column", df.columns)
